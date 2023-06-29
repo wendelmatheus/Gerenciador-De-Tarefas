e7c3c9738591614923e62a36ba9d7da2ac4c6dc6
wendelmatheus <wendeltel6a@gmail.com>
2023-06-29 11:27:13 -0300

    Página já remove itens listados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Menu from "@/view/components/Menu";
 import Head from "next/head";
+import router from "next/router";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -29,6 +30,34 @@ export default function Home() {
     //console.log(lista);
   }, []);
 
+  function handleClickRemover(id: number) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("id", `${id}`);
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: urlencoded,
+    };
+
+    fetch("/api/deletar", requestOptions)
+      .then((response) => {
+        if (response.status == 200) {
+          alert("Item removido com sucesso!");
+          router.reload();
+        } else if (response.status == 404) {
+          alert("Item não encontrado ou já removido!");
+        } else {
+          alert("Algum erro inesperado aconteceu!");
+        }
+      })
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  }
+
   return (
     <div>
       <Head>
@@ -45,7 +74,25 @@ export default function Home() {
           <div className="">
             <div className="bg-blue-400 text-white flex justify-between m-2 p-2">
               {item.title}
-              <input type="checkbox" />
+              <div className="flex flex-row">
+                <input type="checkbox" />
+                <svg
+                  onClick={() => handleClickRemover(item.id)}
+                  xmlns="http://www.w3.org/2000/svg"
+                  color="red"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-4 h-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                  />
+                </svg>
+              </div>
             </div>
           </div>
         ))}
